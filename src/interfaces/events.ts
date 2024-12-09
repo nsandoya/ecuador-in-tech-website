485357a85a99d84f97596db143abb66c6ceadf4f
Nataly Sandoya <121143060+nsandoya@users.noreply.github.com>
2024-12-09 18:14:05 -0500

    Community and Event card components #9 (#17)
    * Create CardCommunity.astro component (including interfaces and implementation in community page)
    
    * Create CardEvent.astro component (including interfaces and implementation in events page)
    
    * Save interfaces in separated files
    
    * Update src/interfaces/events.ts
    
    Co-authored-by: Iván Manzaba <66442822+IvanM9@users.noreply.github.com>
    
    * Create CardBase, refactor CardEvent and CardCommunity
    
    ---------
    
    Co-authored-by: Iván Manzaba <66442822+IvanM9@users.noreply.github.com>

---
+++ src/interfaces/events.ts
@@ -0,0 +1,11 @@
+export interface Event {
+    title: string,
+    description: string,
+    link: string,
+    date: string,
+    place: string,
+    modality: string,
+    hostedBy: string,
+    image: string,
+    attendees: number
+}
